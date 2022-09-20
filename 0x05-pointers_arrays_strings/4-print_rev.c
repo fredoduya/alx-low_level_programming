1de1c6420367c8b31de7fbf458d138df1de95479
fredoduya <fredoduya@gmail.com>
2022-09-20 15:02:08 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * print_rev - reverse a string
  * @s: input string
  * Return: Always 0.
  */
@@ -11,6 +11,7 @@
 void print_rev(char *s)
 {
 	int len = strlen(s);
+
 	while (len--)
 		putchar(*(s + len));
 	putchar(10);
