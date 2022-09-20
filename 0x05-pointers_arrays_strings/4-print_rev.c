62d413c88d61a92ce2f32542dcb8b9360df334d1
fredoduya <fredoduya@gmail.com>
2022-09-20 14:59:24 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,17 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
+
 /**
  * main - check the code
- *
+ * @s: input string
  * Return: Always 0.
  */
 
 void print_rev(char *s)
 {
 	int len = strlen(s);
-
 	while (len--)
-
 		putchar(*(s + len));
 	putchar(10);
-
 }
