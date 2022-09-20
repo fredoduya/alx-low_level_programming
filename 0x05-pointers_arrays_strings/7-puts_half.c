0b47df94e8c7329d68d7baab948351fc50baa933
fredoduya <fredoduya@gmail.com>
2022-09-20 15:21:50 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,13 +14,13 @@ void puts_half(char *str)
 			int oE = 0;
 
 				if (_strlen(str) % 2 != 0)
-						{
-									oE += 1;
-										}
-					for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
-							{
-										_putchar(str[i]);
-											}
-						_putchar('\n');
+		{
+				oE += 1;
+		}
+		for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
+		{
+			putchar(str[i]);
+		}
+		putchar('\n');
 }
 
